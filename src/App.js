2bf56cf7e1f2f5ba3ae1cbd69d316ff005e9c44a
Erasmo <erasmo@Tick.Local>
2021-01-26 00:17:41 +0100

    starting with backend

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Skylab Rocks</h1>
+        <pre>...to the Moon and beyond</pre>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
